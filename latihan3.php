d2ea14fe52cfd03165729946d8895043f3d352c3
vecoll3030 <vecoll3030@gmail.com>
2026-04-28 20:41:33 +0700

    Add files via upload

---
+++ latihan3.php
@@ -0,0 +1,23 @@
+<?php
+$destination = "Tokyo";
+
+switch ($destination) {
+    case "Las Vegas":
+        echo "Bawalah tambahan $500";
+        break;
+    case "Amsterdam":
+        echo "Bawalah pikiran yang terbuka";
+        break;
+    case "Egypt":
+        echo "Bawalah 15 botol tabir surya SPF 50";
+        break;
+    case "Tokyo":
+        echo "Bawalah banyak uang";
+        break;
+    case "Caribbean Islands":
+        echo "Bawalah baju renang";
+        break;
+    default:
+        echo "Tujuan tidak dikenal";
+}
+?>
